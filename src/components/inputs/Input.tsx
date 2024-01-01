f7eeb931ba664a15c01689dc487603c7f39c265d
fahadtufail007 <fahadtufail873@gmail.com>
2024-01-01 17:41:52 +0500

    added input component

---
+++ src/components/inputs/Input.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, TextInput } from 'react-native';
+
+import { colors } from '../../utils/theme';
+
+interface props {
+  placeholder: string,
+  value: any,
+  onChangeText: any,
+  editable:any,
+}
+
+const Input = ({ editable,placeholder, value, onChangeText }: props) => {
+  return (
+    <TextInput style={styles.input} placeholder={placeholder} inputMode="text" placeholderTextColor={colors.darkGreyColor} value={value} onChangeText={onChangeText} editable={editable} />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    backgroundColor: colors.greyColor,
+    color: colors.blackColor,
+    height: 56,
+    padding: 12,
+    borderRadius: 12,
+    fontSize: 18,
+    lineHeight: 24,
+    marginTop: 20,
+    fontWeight: '500',
+  }
+});
+
+export default Input;
